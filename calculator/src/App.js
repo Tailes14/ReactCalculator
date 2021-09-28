a00549e1480656c3448ba026739bec48e83c007b
Tailes14 <54458979+Tailes14@users.noreply.github.com>
2021-09-28 19:50:31 -0400

    added pictures

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -6,17 +6,17 @@ import './App.css';
 function NumberButtons() {
   return <>
   <div className="Numbers">
-  <div className = "numberButton" id="one"></div>
-    <div className = "numberButton"></div>
-    <div className = "numberButton"></div>
-    <div className = "numberButton"></div>
-    <div className = "numberButton"></div>
-    <div className = "numberButton"></div>
-    <div className = "numberButton"></div>
-    <div className = "numberButton"></div>
-    <div className = "numberButton"></div>
-    <div className = "numberButtonZero"></div>
-    <div className = "numberButton"></div>
+  <div className = "numberButton" id="seven"></div>
+    <div className = "numberButton" id="eight"></div>
+    <div className = "numberButton" id="nine"></div>
+    <div className = "numberButton" id="four"></div>
+    <div className = "numberButton" id="five"></div>
+    <div className = "numberButton" id="six"></div>
+    <div className = "numberButton" id="one"></div>
+    <div className = "numberButton" id="two"></div>
+    <div className = "numberButton" id="three"></div>
+    <div className = "numberButtonZero" id="zero"></div>
+    <div className = "numberButton" id="decimal"></div>
   </div>
   </>
 }
@@ -24,12 +24,12 @@ function NumberButtons() {
 function FunctionButtons() {
   return <>
   <div className="Functions">
-    <div className = "functionButton"></div>
-    <div className = "functionButton"></div>
-    <div className = "functionButton"></div>
-    <div className = "functionButton"></div>
-    <div className = "functionButton"></div>
-    <div className = "functionButton"></div>
+    <div className = "functionButton" id="clear"></div>
+    <div className = "functionButton" id="division"></div>
+    <div className = "functionButton" id="multiplication"></div>
+    <div className = "functionButton" id="minus"></div>
+    <div className = "functionButton" id="addition"></div>
+    <div className = "functionButton" id="equal"></div>
   </div>
   </>
 }
